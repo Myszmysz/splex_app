578030702def954a5d13a7732b0f6d11aa781f42
Myszmysz <maciejptgorczyca@gmail.com>
2021-10-06 03:05:41 +0200

    update

--- splex.py
+++ splex.py
@@ -1,4 +1,40 @@
 #! python3
 # splex.py - Application for splitting common expenses within a group.
 
-print("hello world")
+import sys
+import os
+
+
+# todo save splex to file
+# todo read splex from file
+
+def list_of_files():
+    return [name.rstrip(".csv") for name in os.listdir(r"E:\stuff\Maciej\_PYTHON\apps\splex_app\data")]
+
+
+def menu():
+    # todo read list of files
+
+    command = sys.argv[1]
+    if command == "new":
+        None
+        # todo create new splex
+    if command == "open":
+        None
+        # todo open existing splex
+    if command == "list":
+        # print list of splex files
+        lof = list_of_files()
+        if lof:
+            print(*lof, sep="\n")
+        else:
+            print("No splex files found!")
+    if command == "delete":
+        None
+        # todo delete splex
+    if command in list_of_files():
+        # todo add new record
+        # todo calculate result
+
+
+menu()
